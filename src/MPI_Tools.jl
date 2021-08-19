420018be04d7f3610c6b887c0f64395af3f9e1ed
ericamason <ericaellismason@gmail.com>
2021-08-19 12:46:43 -0400

    add units to constants and UL func

--- src/MPI_Tools.jl
+++ src/MPI_Tools.jl
@@ -1,19 +1,22 @@
 module MPI_Tools
 
 using FFTW, Optim, HDF5, PyPlot
-const μ₀ = 4*π*1e-7
-const ϵ₀ = 8.85e-12
+const μ₀ = (4*π*1e-7)u"T*m/A"
+const ϵ₀ = (8.85e-12)u"F/m"
 mu0 = μ₀
 include("MyFFT.jl")
 include("THD.jl")
 include("MyBisect.jl")
 include("meshgrid.jl")
+UL(X) = X |> NoUnits
+
 export meshgrid,
        FFT_1s,
        FFT_1s_to2s,
        μ₀,mu0,ϵ₀,
        mybisect,
-       THD_TimeSeries
+       THD_TimeSeries,
+       UL
 
 
 # Write your package code here.
